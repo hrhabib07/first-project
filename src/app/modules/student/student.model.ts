f2201851555141d3c867b5e3adefeb158388883a
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-26 00:35:10 +0600

    module 9 class 10 completed

--- src/app/modules/student/student.model.ts
+++ src/app/modules/student/student.model.ts
@@ -134,6 +134,17 @@ studentSchema.post("save", function () {
 // query middleware 
 studentSchema.pre("find", function (next) {
   // console.log(this);
+  this.find({ isDeleted: { $ne: true } })
+  next()
+})
+studentSchema.pre("findOne", function (next) {
+  // console.log(this);
+  this.find({ isDeleted: { $ne: true } })
+  next()
+})
+
+studentSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { isDeleted: { $ne: true } } })
   next()
 })
 
--- src/app/modules/student/student.service.ts
+++ src/app/modules/student/student.service.ts
@@ -21,7 +21,10 @@ const getAllStudentFromDB = async () => {
 };
 
 const getSingleStudentFromDB = async (id: string) => {
-  const result = await Student.findOne({ id });
+  // const result = await Student.findOne({ id });
+  const result = await Student.aggregate([
+    { $match: { id: id } }
+  ])
   return result;
 };
 const deleteStudentFromDB = async (id: string) => {

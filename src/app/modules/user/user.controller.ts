9a4d164e4f52f92a8e2da9d5a4a3e3244cc4cf0c
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-27 16:03:48 +0600

    custome mddilware added

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -2,21 +2,22 @@ import { NextFunction, Request, RequestHandler, Response } from "express";
 import { userServices } from "./user.service";
 import httpStatus from "http-status";
 import sendResponse from "../../utils/sendResponse";
-const createUser: RequestHandler = async (req, res, next) => {
-    try {
-        const { password, student: studentData } = req.body;
-        // const zodParsedData = userValidation.userValidationSchema.parse(userData);
-        const result = await userServices.createUserIntoDB(password, studentData);
-        sendResponse(res, {
-            statusCode: httpStatus.OK,
-            success: true,
-            message: "Student is created successfully",
-            data: result
-        })
-    } catch (error) {
-        next(error);
+const catchAsync = (fn: RequestHandler) => {
+    return (req: Request, res: Response, next: NextFunction) => {
+        Promise.resolve(fn(req, res, next)).catch((error) => next(error))
     }
-};
+}
+const createUser: RequestHandler = catchAsync(async (req, res, next) => {
+    const { password, student: studentData } = req.body;
+    // const zodParsedData = userValidation.userValidationSchema.parse(userData);
+    const result = await userServices.createUserIntoDB(password, studentData);
+    sendResponse(res, {
+        statusCode: httpStatus.OK,
+        success: true,
+        message: "Student is created successfully",
+        data: result
+    })
+});
 export const userController = {
     createUser
 }
--- src/app/modules/user/user.route.ts
+++ src/app/modules/user/user.route.ts
@@ -1,9 +1,14 @@
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
 import { userServices } from './user.service';
 import { userController } from './user.controller';
 
 const router = express.Router();
 
-router.post("/create-student", userController.createUser);
+const senaBahini = (req: Request, res: Response, next: NextFunction) => {
+    console.log("I am a senabashini");
+    next()
+}
+
+router.post("/create-student", senaBahini, userController.createUser);
 
 export const UserRoute = router;

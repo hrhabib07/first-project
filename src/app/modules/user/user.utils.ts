95a5053869606d33c9c588a98d0b0e52baf78060
Hr Habib <mdhabibur.hr7@gmail.com>
2024-05-29 12:01:02 +0600

    validate students by semester implemented m13c1

--- src/app/modules/user/user.utils.ts
+++ src/app/modules/user/user.utils.ts
@@ -11,13 +11,19 @@ const findLastStudent = async () => {
         _id: 0,
     }
     ).sort({ createdAt: -1 }).lean();
-    return lastStudent?.id ? lastStudent?.id.substring(6) : undefined;
+    return lastStudent?.id ? lastStudent?.id : undefined;
 }
 
 export const generateStudentId = async (payLoad: TAcademicSemester) => {
     // first time
-    const currentId = await findLastStudent() || (0).toString();
-
+    let currentId: string | undefined = (0).toString();
+    const lastStudentId = await findLastStudent();
+    const lastAcademicYear = lastStudentId?.substring(0, 4);
+    console.log();
+    const lastAcademicSemester = lastStudentId?.substring(4, 6);
+    if (lastAcademicSemester === payLoad.code && lastAcademicYear === payLoad.year) {
+        currentId = lastStudentId?.substring(6);
+    }
     let incrementId = (Number(currentId) + 1).toString().padStart(4, "0");
     incrementId = `${payLoad.year}${payLoad.code}${incrementId}`
     return incrementId;

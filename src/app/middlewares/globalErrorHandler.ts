d49feec048f409a75cc659e438e9195bfc9582e8
Hr Habib <mdhabibur.hr7@gmail.com>
2024-06-05 10:15:35 +0600

    error handling started

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,11 +1,30 @@
 import { NextFunction, Request, Response } from "express";
+import { ZodError } from "zod";
 
 const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-    const statusCode = error.statusCode || 500;
-    const message = error.message || "something went wrong";
+    let statusCode = error.statusCode || 500;
+    let message = error.message || "something went wrong";
+
+    type TErrorSource = {
+        path: string | number;
+        message: string
+    }[];
+    const errorSource: TErrorSource = [
+        {
+            path: "",
+            message: "something went wrong"
+        }
+    ];
+
+    if (error instanceof ZodError) {
+        statusCode = 400;
+        message = "ami zod error "
+    }
+
     return res.status(statusCode).json({
         success: false,
         message,
+        errorSource,
         error
     })
 };

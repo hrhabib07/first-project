39cc7e20d1f0fd059051c14fc49d844f1343f11b
Hr Habib <mdhabibur.hr7@gmail.com>
2024-06-05 13:54:02 +0600

    zod validation error handled

--- src/app/middlewares/globalErrorHandler.ts
+++ src/app/middlewares/globalErrorHandler.ts
@@ -1,5 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { ZodError } from "zod";
+import { stat } from "fs";
+import { ZodError, ZodIssue, } from "zod";
+import config from "../config";
 
 const globalErrorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
     let statusCode = error.statusCode || 500;
@@ -9,23 +11,41 @@ const globalErrorHandler = (error: any, req: Request, res: Response, next: NextF
         path: string | number;
         message: string
     }[];
-    const errorSource: TErrorSource = [
+    let errorSource: TErrorSource = [
         {
             path: "",
             message: "something went wrong"
         }
     ];
 
+    const handleZodError = (error: ZodError) => {
+        const statusCode = 400;
+        const errorSources: TErrorSource = error.issues.map((issue: ZodIssue) => {
+            return {
+                path: issue?.path[issue.path.length - 1],
+                message: issue.message,
+            };
+        });
+        return {
+            statusCode,
+            message: "validation error",
+            errorSources
+        }
+
+    }
+
     if (error instanceof ZodError) {
-        statusCode = 400;
-        message = "ami zod error "
+        const simplifiedError = handleZodError(error);
+        statusCode = simplifiedError.statusCode;
+        message = simplifiedError.message;
+        errorSource = simplifiedError.errorSources
     }
 
     return res.status(statusCode).json({
         success: false,
         message,
         errorSource,
-        error
+        stack: config.NODE_ENV === "development" ? error.stack : null,
     })
 };
 

5baa8e99826b6edae0687dbae44fd241e2477a6a
Hr Habib <mdhabibur.hr7@gmail.com>
2024-06-10 11:31:19 +0600

    semester registration interface, model, validation created

---
+++ src/app/modules/semesterRegistration/semesterRegistration.model.ts
@@ -0,0 +1,16 @@
+import mongoose, { Schema, model } from "mongoose";
+import { TSemesterRegistration } from "./semesterRegistration.interface";
+import { SemesterRegistrationStatus } from "./semesterRegistration.constant";
+
+const semesterRegistrationSchema = new Schema<TSemesterRegistration>(
+    {
+        academicSemester: { type: Schema.Types.ObjectId, required: true, unique: true },
+        status: { type: String, enum: SemesterRegistrationStatus, default: "UPCOMING" },
+        startDate: { type: Date, required: true, unique: true },
+        endDate: { type: Date, required: true, unique: true },
+        minCredit: { type: Number, default: 3 },
+        maxCredit: { type: Number, default: 16 }
+    }, { timestamps: true }
+);
+
+export const SemesterRegistration = mongoose.model<TSemesterRegistration>("SemesterRegistration", semesterRegistrationSchema);

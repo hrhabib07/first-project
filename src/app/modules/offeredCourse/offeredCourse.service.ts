6009580756d8b81f1ca68cd26cb3cccd08269621
Hr Habib <mdhabibur.hr7@gmail.com>
2024-06-12 14:06:44 +0600

    offered course validation implemented

--- src/app/modules/offeredCourse/offeredCourse.service.ts
+++ src/app/modules/offeredCourse/offeredCourse.service.ts
@@ -1,8 +1,25 @@
+import httpStatus from "http-status";
+import AppError from "../../errors/appError";
+import { SemesterRegistration } from "../semesterRegistration/semesterRegistration.model";
 import { TOfferedCourse } from "./offeredCourse.interface";
 import { OfferedCourse } from "./offeredCourse.model";
+import { AcademicDepartment } from "../academicDepartment/academicDepartment.model";
+import { Types } from "mongoose";
 
 const createOfferedCourseIntoDB = async (payload: TOfferedCourse) => {
-    const result = await OfferedCourse.create(payload);
+    const { semesterRegistration, academicDepartment, academicFaculty, faculty, course } = payload;
+    const isSemesterRegistrationExist = await SemesterRegistration.findById(semesterRegistration);
+    if (!isSemesterRegistrationExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "semester Registration not found")
+    };
+    const academicSemester = isSemesterRegistrationExist?.academicSemester;
+
+    const isAcademicDepartmentExist = await AcademicDepartment.findById(academicDepartment);
+    if (!isSemesterRegistrationExist) {
+        throw new AppError(httpStatus.BAD_REQUEST, "Academic Department not found")
+    };
+
+    const result = await OfferedCourse.create({ ...payload, academicSemester });
     return result;
 };
 
